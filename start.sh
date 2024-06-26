5116372f59603813ab184ca869d0c3d35d76c455
Tony Meyer <tony.meyer@canonical.com>
2024-06-27 09:44:11 +1200

    Add the base layer as well.

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 PEBBLE_DIR=~/code/pebble
+MY_DIR=$(pwd)
 
 export TERM=xterm
+export PEBBLE=/tmp/pebble
 
 tmux new-session -d -s simulator
 
@@ -11,9 +13,10 @@ tmux split-window -t simulator:0 -v
 tmux split-window -t simulator:0 -v
 tmux select-layout -t simulator:0 tiled
 
-tmux send-keys -t simulator:0.0 "cd $PEBBLE_DIR && PEBBLE=/tmp/pebble go run ./cmd/pebble run" C-m
+tmux send-keys -t simulator:0.0 "cd $PEBBLE_DIR && go run ./cmd/pebble run" C-m
 tmux send-keys -t simulator:0.1 "watch 'tail charm.log -n 10'" C-m
 tmux send-keys -t simulator:0.2 'go run sim.go' C-m
+tmux send-keys -t simulator:0.3 "cd $PEBBLE_DIR && go run ./cmd/pebble add base $MY_DIR/base.yaml && cd $MY_DIR" C-m
 tmux send-keys -t simulator:0.3 'python3 -m http.server 8080' C-m
 
 tmux attach-session -t simulator
