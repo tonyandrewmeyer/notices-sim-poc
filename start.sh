9d6bacc3a0c128f4f85b4d885dc91efafdd20580
Tony Meyer <tony.meyer@canonical.com>
2024-07-01 15:14:53 +1200

    Add a small delay to make it more likely Pebble is up.

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ tmux select-layout -t simulator:0 tiled
 tmux send-keys -t simulator:0.0 "cd $PEBBLE_DIR && go run ./cmd/pebble run" C-m
 tmux send-keys -t simulator:0.1 "watch 'tail charm.log -n 10'" C-m
 tmux send-keys -t simulator:0.2 'go run sim.go' C-m
-tmux send-keys -t simulator:0.3 "cd $PEBBLE_DIR && go run ./cmd/pebble add base $MY_DIR/base.yaml && cd $MY_DIR" C-m
+tmux send-keys -t simulator:0.3 "sleep 0.1 && cd $PEBBLE_DIR && go run ./cmd/pebble add base $MY_DIR/base.yaml && cd $MY_DIR" C-m
 tmux send-keys -t simulator:0.3 'python3 -m http.server 8080' C-m
 
 tmux attach-session -t simulator
